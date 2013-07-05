5181523a51eae72a66aa3d2aa7562107b7cd874f
Alex Kesling <alex.kesling@gmail.com>
2013-07-05 14:25:29 -0400

    Video with basic overlay works!

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ cv.NamedWindow('a_window', cv.CV_WINDOW_AUTOSIZE)
 webcam=cv.CaptureFromCAM(0)
 font = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 3, 8) #Creates a font
 
-image=cv.QueryFrame(webcam)
-cv.PutText(image,"Hello World!!!", (x,y),font, 255) #Draw the text
-cv.ShowImage('a_window', image) #Show the image
-cv.WaitKey(10000)
-cv.SaveImage('image.png', image) #Saves the image
+while True:
+    image=cv.QueryFrame(webcam)
+    cv.PutText(image,"Hello World!!!", (x,y),font, 255) #Draw the text
+    cv.ShowImage('a_window', image) #Show the image
+    cv.WaitKey(2)
+    cv.SaveImage('image.png', image) #Saves the image
