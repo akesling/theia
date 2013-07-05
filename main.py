d91220ea29b6e866237d66efa41a52e4b078ddab
Alex Kesling <alex.kesling@gmail.com>
2013-07-05 14:24:09 -0400

    Webcam capture now! Next up... VIDEO!

--- main.py
+++ main.py
@@ -3,13 +3,16 @@
 # Initial tutorial turned to (http://www.neuroforge.co.uk/index.php/getting-started-with-python-a-opencv)
 import cv
 
-cv.NamedWindow('a_window', cv.CV_WINDOW_AUTOSIZE)
-image=cv.LoadImage('picture.png', cv.CV_LOAD_IMAGE_COLOR) #Load the image
-font = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 3, 8) #Creates a font
 x = 255
 y = 255
+
+# Setup
+cv.NamedWindow('a_window', cv.CV_WINDOW_AUTOSIZE)
+webcam=cv.CaptureFromCAM(0)
+font = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 3, 8) #Creates a font
+
+image=cv.QueryFrame(webcam)
 cv.PutText(image,"Hello World!!!", (x,y),font, 255) #Draw the text
 cv.ShowImage('a_window', image) #Show the image
 cv.WaitKey(10000)
 cv.SaveImage('image.png', image) #Saves the image
-
